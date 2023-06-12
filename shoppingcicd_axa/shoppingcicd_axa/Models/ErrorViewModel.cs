580c281c347ecb535baa13c5d253641f344f58f7
Nikhil <nikhilshah.net@hotmail.com>
2023-06-12 10:23:58 +0530

    New Project created

---
+++ shoppingcicd_axa/shoppingcicd_axa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shoppingcicd_axa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
